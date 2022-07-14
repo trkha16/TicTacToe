88ba8f6c3b9e377246f1930f116a6e286102ea06
trkha16 <tbkha1609@gmail.com>
2022-07-14 08:24:22 +0700

    Init

---
+++ src/components/TicTacToe.js
@@ -0,0 +1,9 @@
+import styles from "./TicTacToe.module.scss";
+
+export default function TicTacToe() {
+    return (
+        <div className={styles.root}>
+            <div className={styles.container}></div>
+        </div>
+    );
+}
